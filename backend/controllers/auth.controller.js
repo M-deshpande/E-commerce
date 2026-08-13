e21518c5117553c5e54c46a651674a42ebf84303
Madhushri Deshpande <madhushry12@gmail.com>
2026-08-14 00:03:12 +0530

    Implement login functionality with token generation and cookie management

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -67,7 +67,29 @@ export const signup = async (req, res) => {
 }
 
 export const login = async (req, res) => {
-  res.send("Login route called");
+  const {email, password} = req.body;
+  try {
+    const user = await User.findOne({email});
+    if (user && await (user.comparePassword(password))) {
+
+      const {accessToken, refreshToken} = generateTokens(user._id);
+      await storeRefreshToken(user._id, refreshToken);
+
+      setCookies(res, accessToken, refreshToken);
+
+      res.json({
+          id: user._id,
+          name: user.name,
+          email: user.email,
+          role: user.role
+      });
+      
+    } else {
+      res.status(401).json({message: "Invalid email or password"});
+    }
+  } catch (error) {
+    res.status(500).json({message: "Error logging in", error :error.message});
+  }
 }
 
 export const logout = async (req, res) => {
