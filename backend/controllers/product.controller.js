d79843f310b79e09402fe8b9bc082ddb78065d82
Madhushri Deshpande <madhushry12@gmail.com>
2026-08-20 18:18:54 +0530

    Implement delete product functionality

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -57,4 +57,40 @@ export const createProduct = async (req, res) => {
     console.log("Error in createProduct controller", error.message);
     res.status(500).json({ message: "Server error", error: error.message  })
   }
-}
+};
+
+export const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    if (product.image) {   
+      const publicId = product.image.split("/").pop().split(".")[0];
+      try {
+        await cloudinary.uploader.destroy(`products/${publicId}`);
+        console.log("Image deleted from Cloudinary");
+      } catch (error) {
+        console.log("Error deleting image from Cloudinary", error.message);
+      }
+    }
+
+    await product.findByIdAndDelete(req.params.id);
+
+    res.json({ message: "Product deleted successfully" });
+
+  } catch (error) {
+    console.log("Error in deleteProduct controller", error.message);
+    res.status(500).json({ message: "Server error", error: error.message  })
+  }
+};
+
+const getRecommendedProducts = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    console.log("Error in getRecommendedProducts controller", error.message);
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
