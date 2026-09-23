170c8f4eb9dca9a2da6483db44c3649784a898d2
Madhushri Deshpande <madhushry12@gmail.com>
2026-09-24 00:24:48 +0530

    Add validateCoupon functionality to check coupon validity and expiration

--- backend/controllers/coupon.controller.js
+++ backend/controllers/coupon.controller.js
@@ -13,3 +13,26 @@ export const getCoupon = async (req, res) => {
     }
 };
 
+export const validateCoupon = async (req, res) => {
+    try {
+        const { code } = req.body;
+        const coupon = await Coupon.findOne({ code: code, userId: req.user._id, isActive: true });
+        if (!coupon) {
+            return res.status(404).json({ message: "Coupon not found or inactive" });
+        }
+
+        if (coupon.expirationDate < new Date()) {
+            coupon.isActive = false;
+            await coupon.save();
+            return res.status(400).json({ message: "Coupon has expired" });
+        }
+        res.json({
+            message: "Coupon is valid",
+            code : coupon.code,
+            discountPercentage: coupon.discountPercentage,
+        });
+    } catch (error) {
+        console.log("Error in validateCoupon controller", error.message);
+        res.status(500).json({ message: error.message });
+    }
+};
